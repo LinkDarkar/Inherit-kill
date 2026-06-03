4aba9171965538cfa535d66a54d54ae6d90019a5
Bryam <86389156+athersoft@users.noreply.github.com>
2026-06-02 23:09:33 -0400

    Correo en el logger

--- Menus/PantallaRegistro.cs
+++ Menus/PantallaRegistro.cs
@@ -15,18 +15,19 @@ public partial class PantallaRegistro : Control
 	// Conecta la señal 'pressed' de tu botón a este método
 	public void _on_boton_empezar_pressed()
 	{
-		// Validamos que no dejen los campos vacíos
 		if (inputRut.Text == "" || inputCorreo.Text == "")
 		{
 			GD.Print("Por favor, ingrese todos los datos.");
 			return;
 		}
 
-		// Guardamos el ID del jugador en nuestro Singleton global
-		// Puedes concatenar el RUT y el Correo si lo deseas
+		// Enviamos el RUT
 		LoggerDatos.Instance.IdJugador = inputRut.Text;
+		
+		// Enviamos el Correo
+		LoggerDatos.Instance.CorreoJugador = inputCorreo.Text;
 
-		// Cambiamos a la escena de tu Menú Principal o directamente al Nivel 1
-		GetTree().ChangeSceneToFile("res://Menus/menu_principal.tscn"); 
+		// Cambiamos a la escena del nivel
+		GetTree().ChangeSceneToFile("res://Menus/menu_principal.tscn"); // Ajusta a tu ruta real
 	}
 }
